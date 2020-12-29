2e59fa391cd85e7722e4917b29576ace08d19cd1
limz <limzgiser@163.com>
2020-12-29 15:31:14 +0800

    'mapboxgl-draw'

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import mapboxgl from "mapbox-gl";
 import "../node_modules/mapbox-gl/dist/mapbox-gl.css";
-import { MapboxLayer } from "@deck.gl/mapbox";
-import { EditableGeoJsonLayer } from "@nebula.gl/layers";
-import { DrawPolygonMode } from "@nebula.gl/edit-modes";
-import { Deck } from "@deck.gl/core";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+import MapboxDraw from "@mapbox/mapbox-gl-draw";
+import DrawRectangle from 'mapbox-gl-draw-rectangle-mode';
+
+import {
+  CircleMode,
+  DragCircleMode,
+  DirectMode,
+  SimpleSelectMode
+} from 'mapbox-gl-draw-circle';
 
 const INITIAL_VIEW_STATE = {
   latitude: 51.47,
@@ -19,59 +25,47 @@ mapboxgl.accessToken =
 var map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v11",
-  interactive: false,
+  // interactive: false,
   center: [INITIAL_VIEW_STATE.longitude, INITIAL_VIEW_STATE.latitude],
   zoom: INITIAL_VIEW_STATE.zoom,
   bearing: INITIAL_VIEW_STATE.bearing,
   pitch: INITIAL_VIEW_STATE.pitch,
 });
+var draw = new MapboxDraw({
+  modes: {
+    ...MapboxDraw.modes,
+    draw_circle  : CircleMode,
+    drag_circle  : DragCircleMode,
+    draw_rectangle :DrawRectangle,
+    direct_select: DirectMode,
+    simple_select: SimpleSelectMode
+  }
+});
 
-var myFeatureCollection = {
-  type: "FeatureCollection",
-  features: [],
+map.addControl(draw);
+
+map.on("draw.create", updateArea);
+map.on("draw.delete", updateArea);
+map.on("draw.update", updateArea);
+
+function updateArea(e) {
+  console.log(e);
+}
+
+document.getElementById("test01").onclick = function (e) {
+  draw.changeMode("draw_point");
 };
 
-const deck = new Deck({
-  canvas: "deck-canvas",
-  width: "100%",
-  height: "100%",
-  initialViewState: INITIAL_VIEW_STATE,
-  controller: true,
-  onViewStateChange: ({ viewState }) => {
-    map.jumpTo({
-      center: [viewState.longitude, viewState.latitude],
-      zoom: viewState.zoom,
-      bearing: viewState.bearing,
-      pitch: viewState.pitch,
-    });
-  },
-  layers: getLayers(),
-});
-function getLayers() {
-  const layers = [
-    new EditableGeoJsonLayer({
-      id: "nebula",
-      data: myFeatureCollection,
-      selectedFeatureIndexes: [],
-      mode: DrawPolygonMode,
-      // Styles
-      filled: true,
-      pointRadiusMinPixels: 2,
-      pointRadiusScale: 2000,
-      extruded: true,
-      getElevation: 1000,
-      getFillColor: [200, 0, 80, 180],
+document.getElementById("test02").onclick = function (e) {
+  draw.changeMode("draw_line_string");
+};
 
-      // Interactive props
-      pickable: true,
-      autoHighlight: true,
-
-      onEdit: ({ updatedData, editType, featureIndexes, editContext }) => {
-        myFeatureCollection = updatedData;
-        deck.setProps({ layers: getLayers() });
-      },
-    }),
-  ];
-
-  return layers;
-}
+document.getElementById("test03").onclick = function (e) {
+  draw.changeMode("draw_polygon");
+};
+document.getElementById("test04").onclick = function (e) {
+  draw.changeMode("drag_circle");
+};
+document.getElementById("test05").onclick = function (e) {
+  draw.changeMode("draw_rectangle");
+};
